70011d9dba237be37927838cda080cfd7412a7be
Vendetta1871 <156649586+Vendetta1871@users.noreply.github.com>
2025-06-01 21:13:07 +0800

    Issue#2_v0.1
    Initial realization of a Weighted Orientied Graphs (covered by tests).

---
+++ EvoGraph/Graph/Graph.cs
@@ -0,0 +1,103 @@
+﻿using System.Numerics;
+
+namespace EvoGraph.Graph;
+
+
+public class Graph
+{
+    public int Count { get; set; }
+    public double[,] AdjacencyMatrix;
+    
+    /// <summary>
+    /// Create an empty graph with <b>size</b> nodes.
+    /// </summary>
+    public Graph(int size)
+    {
+        Count = size;
+        AdjacencyMatrix = new double[size, size];
+        for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            AdjacencyMatrix[i, j] = -1; 
+    }
+
+    /// <summary>
+    /// Add a new node to the graph.
+    /// </summary>
+    public void AddNode()
+    {
+        double[,] matrix = new double[Count + 1, Count + 1];
+        for (int i = 0; i < Count; i++)
+        {
+            for (int j = 0; j < Count; j++)
+                matrix[i, j] = AdjacencyMatrix[i, j];
+            matrix[i, Count] = -1;
+        }
+        
+        for (int i = 0; i < Count; i++)
+            matrix[Count, i] = -1;
+        matrix[Count, Count] = -1;
+        Count = Count + 1;
+        AdjacencyMatrix = matrix;
+    }
+
+    /// <summary>
+    /// Add a new edge to graph or modify existed.
+    /// </summary>
+    /// <param name="from">
+    /// <b>X</b> coordinate in adjacency matrix.
+    /// </param>
+    /// <param name="to">
+    /// <b>Y</b> coordinate in adjacency matrix.
+    /// </param>
+    /// <param name="weight">
+    /// The weight of an edge
+    /// </param>
+    public void AddEdge(int from, int to, double weight)
+    {
+        if (from < 0 || from >= Count || to < 0 || to >= Count) 
+            throw new ArgumentOutOfRangeException();
+        AdjacencyMatrix[from, to] = weight; 
+    }
+
+    /// <summary>
+    /// Convert the graph to a string for serialization.
+    /// </summary>
+    public override string? ToString()
+    {
+        string str = Count + "\n";
+        for (int i = 0; i < Count; i++)
+        { 
+            for (int j = 0; j < Count; j++)
+                str += AdjacencyMatrix[i, j] + " ";
+            str += "\n";
+        }
+        
+        return str;
+    }
+    
+    /// <summary>
+    /// Create a graph from string (deserialize).
+    /// </summary>
+    /// <param name="lines">
+    /// Array of strings representing a graph (string split
+    /// by a <b>'\n'</b> symbol). 
+    /// </param>
+    /// <returns>
+    /// The new graph.
+    /// </returns>
+    public static Graph FromString(string[] lines)
+    {
+        var count = int.Parse(lines[0]);
+        var graph = new Graph(count);
+        for (int i = 1; i <= count; i++)
+        {
+            string[] values = lines[i].Trim().Split(' ');
+            if (values.Length != count) 
+                throw new Exception("Incorrect number of values");
+            for (int j = 0; j < count; j++)
+                graph.AdjacencyMatrix[i - 1, j] = double.Parse(values[j]);
+        }
+
+        return graph;
+    }
+}
---
+++ EvoGraphTest/GraphTest/BasicGraphTests.cs
@@ -0,0 +1,55 @@
+﻿using EvoGraph.Graph;
+
+namespace EvoGraphTest.GraphTest;
+
+public class BasicGraphTests
+{
+    
+    private static double[,] _answerMatrix = new double[,]
+    {
+        {-1, 0.2, -1},
+        {2, 5, 4},
+        {8, -1, -1}
+    };
+
+    private static string _answerString = "3\n-1 0,2 -1 \n2 5 4 \n8 -1 -1 \n";
+
+    private Graph GenerateGraph()
+    {
+        var graph = new Graph(2);
+        graph.AddEdge(0, 0, -1);
+        graph.AddEdge(1, 1, 5);
+        graph.AddNode();
+        graph.AddEdge(0, 1, 0.2);
+        graph.AddEdge(0, 2, -1);
+        graph.AddEdge(1, 0, 2);
+        graph.AddEdge(1, 2, 4);
+        graph.AddEdge(2, 0, 8);
+        graph.AddEdge(2, 1, -1);
+        graph.AddEdge(2, 2, -1);
+        return graph;
+    }
+    
+    [Test]
+    public void GraphCreationTest()
+    {
+        var graph = GenerateGraph();
+        Assert.That(graph.AdjacencyMatrix, Is.EqualTo(_answerMatrix));
+    }
+
+    [Test]
+    public void SerializationTest()
+    {
+        var graph = GenerateGraph();
+        var str = graph.ToString();
+        Assert.That(str, Is.EqualTo(_answerString));
+    }
+    
+    [Test]
+    public void DeserializationTest()
+    {
+        var graph = Graph.FromString(_answerString.Split('\n'));
+        Assert.That(graph.AdjacencyMatrix, Is.EqualTo(_answerMatrix));
+    }
+
+}
